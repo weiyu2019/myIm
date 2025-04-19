bb4fbfe85d656cda6b768d0cacbfd2d3f1c7635b
weiyu02 <weiyu02@megvii.com>
2025-04-19 21:56:59 +0800

    makefile

--- deploy/mk/user-rpc.mk
+++ deploy/mk/user-rpc.mk
@@ -5,7 +5,7 @@ SERVER_TYPE=rpc
 
 # 测试环境配置
 # docker的镜像发布地址
-DOCKER_REPO_TEST=registry.cn-hangzhou.aliyuncs.com/my-im-1/${SERVER_NAME}-${SERVER_TYPE}-dev
+DOCKER_REPO_TEST=crpi-g6muopbpy3n1uhbz.cn-hangzhou.personal.cr.aliyuncs.com/my-im-1/${SERVER_NAME}-${SERVER_TYPE}-dev
 # 测试版本
 VERSION_TEST=$(VERSION)
 # 编译的程序名称
