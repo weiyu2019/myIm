aa9dee3f9cff4e179c0b68f59a88c45cb7ba6369
weiyu02 <weiyu02@megvii.com>
2025-04-20 10:20:22 +0800

    etcd

---
+++ deploy/script/user-rpc-test.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+repo_addr='crpi-g6muopbpy3n1uhbz.cn-hangzhou.personal.cr.aliyuncs.com/my-im-1/user-rpc-dev'
+tag='latest'
+
+container_name='myIm-user-rpc-test'
+
+docker stop ${container_name}
+
+docker rm ${container_name}
+
+docker rmi ${container_name}
+
+docker pull ${repo_addr}:${tag}
+
+docker run -p 10000:10000 --name=${container_name} -d ${repo_addr}:${tag}
